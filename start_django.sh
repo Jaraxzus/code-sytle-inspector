b46cdf9583e5f9cc8a5c8563586bfe9335b5c44e
Jaraxzus <alex099024@gmail.com>
2023-08-18 21:42:26 +0300

    fix static files error

--- start_django.sh
+++ start_django.sh
@@ -4,7 +4,7 @@ source .env
 echo "DJANGO_SUPERUSER_PASSWORD: $DJANGO_SUPERUSER_PASSWORD"
 echo "DJANGO_SUPERUSER_USERNAME: $DJANGO_SUPERUSER_USERNAME"
 echo "DJANGO_SUPERUSER_EMAIL : $DJANGO_SUPERUSER_EMAIL"
-python manage.py collectstatic
+# python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput &
